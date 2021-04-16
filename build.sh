a923214ed71c79a3f56667a5e8026ea7778788a8
AlexandraTrifan <Alexandra.Trifan@analog.com>
2021-04-16 12:55:20 +0300

    use test version for libm2k

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 LIBIIO_BRANCH=master
 LIBAD9361_BRANCH=master
-LIBM2K_BRANCH=master
+LIBM2K_BRANCH=kernelbuffers_v1
 GRIIO_BRANCH=upgrade-3.8
 GNURADIO_FORK=analogdevicesinc
 GNURADIO_BRANCH=scopy
