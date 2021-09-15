a1ad3f266673ab687898c5581287ef2c1366421c
Adrian Suciu <adrian.suciu@analog.com>
2021-09-15 21:59:47 +0300

    tree: refactor
    use build script with staging folder
    use submodules
    
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

---
+++ build_system_setup.sh
@@ -0,0 +1,4 @@
+export STAGING=$HOME/staging
+export JOBS="-j9"
+export MINGW_VERSION=mingw64
+export ARCH=x86_64
---
+++ mingw_toolchain.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash.exe
+source build_system_setup.sh
+
+BUILD_STATUS_FILE=/tmp/scopy-mingw-build-status
+touch $BUILD_STATUS_FILE
+
+#TODO: make each dep install it's own deps
+# Exit immediately if an error occurs
+#set -e
+#set -x 
+
+export PATH=/bin:/usr/bin:/${MINGW_VERSION}/bin:/c/Program\ Files/Git/cmd:/c/Windows/System32:/c/Program\ Files/Appveyor/BuildAgent
+
+export WORKDIR=${PWD}
+
+if [ -z "$STAGING" ];
+	then 
+		export STAGING_DIR=$WORKDIR/staging/$MINGW_VERSION
+		export STAGING_ENV=$WORKDIR/staging
+	else
+		export STAGING_DIR=$STAGING/$MINGW_VERSION
+		export STAGING_ENV=$STAGING
+fi
+
+export CC=/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-gcc.exe
+export CXX=/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-g++.exe
+export JOBS="-j 9"
+export MAKE_BIN=/${MINGW_VERSION}/bin/mingw32-make.exe 
+export MAKE_CMD="$MAKE_BIN $JOBS"
+export CMAKE_GENERATOR="Unix Makefiles"
+export CMAKE_OPTS=( \
+	-DCMAKE_C_COMPILER:FILEPATH=${CC} \
+	-DCMAKE_CXX_COMPILER:FILEPATH=${CXX} \
+	-DCMAKE_MAKE_PROGRAM:FILEPATH=${MAKE_BIN}\
+	-DPKG_CONFIG_EXECUTABLE=/$MINGW_VERSION/bin/pkg-config.exe \
+	-DCMAKE_MODULE_PATH=$STAGING_DIR \
+	-DCMAKE_PREFIX_PATH=$STAGING_DIR/lib/cmake \
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DCMAKE_STAGING_PREFIX=$STAGING_DIR \
+	-DCMAKE_INSTALL_PREFIX=$STAGING_DIR \
+)
+export QMAKE="$STAGING_DIR/bin/qmake"
+export CMAKE="/mingw64/bin/cmake ${CMAKE_OPTS[@]} "
+export PACMAN="pacman -r $STAGING_ENV --noconfirm --needed"
+
+export AUTOCONF_OPTS="--prefix=$STAGING_DIR \
+	--host=${ARCH}-w64-mingw32 \
+	--enable-shared \
+	--disable-static"
+
+if [ ${ARCH} == "i686" ]
+then
+	export RC_COMPILER_OPT="-DCMAKE_RC_COMPILER=$WORKDIR/windres/windres.exe"
+else
+	export RC_COMPILER_OPT="-DCMAKE_RC_COMPILER=/mingw64/bin/windres.exe"
+fi
+
+echo -- $STAGING_DIR is the staging dir
+echo -- PATH is $PATH
+echo -- using cmake command
+echo $CMAKE
