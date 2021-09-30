c69deb3d3d6f24776a03532d7d12deac704564f2
Adrian Suciu <adrian.suciu@analog.com>
2021-09-30 13:05:53 +0300

    tree: reduce image size by not using staging for packages installed with pacman
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- init_staging.sh
+++ init_staging.sh
@@ -3,9 +3,12 @@ set -x
 source mingw_toolchain.sh $1
 
 init_staging() {
+	if [ -v USE_STAGING ] 
+	then
 	rm -rf $STAGING_ENV
 	mkdir -p $STAGING_ENV/var/lib/pacman/local
 	mkdir -p $STAGING_ENV/var/lib/pacman/sync
 	/usr/bin/bash.exe -c "$PACMAN -Syuu bash filesystem mintty pacman"
+	fi
 }
 init_staging
